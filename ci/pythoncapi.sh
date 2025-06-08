f54f2ac1271bdbabc373d28f223494acb6f20ab8
Marcelo Duarte <marcelotduarte@users.noreply.github.com>
2025-06-07 23:14:11 -0300

    chore: use prettier and shellcheck

--- ci/pythoncapi.sh
+++ ci/pythoncapi.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 # Get script directory (without using /usr/bin/realpath)
-CI_DIR=$(cd `dirname "${BASH_SOURCE[0]}"` && pwd)
-TOP_DIR=$(cd $CI_DIR/.. && pwd)
+_CI_DIR=$(dirname "${BASH_SOURCE[0]}")
+CI_DIR=$(cd "$_CI_DIR" && pwd)
+TOP_DIR=$(cd "$CI_DIR/.." && pwd)
 
 CI_TMPDIR=$(python -c "import tempfile; print(tempfile.mkdtemp())")
-pushd $CI_TMPDIR >/dev/null
-git clone -q https://github.com/python/pythoncapi-compat
-if [ -d pythoncapi-compat ]; then
-    cd pythoncapi-compat
-    python upgrade_pythoncapi.py $TOP_DIR/src/ --download $TOP_DIR/src/freeze_core/include/
-fi
-popd >/dev/null
+pushd "$CI_TMPDIR" >/dev/null || exit
+git clone -q https://github.com/python/pythoncapi-compat .
+python upgrade_pythoncapi.py \
+    "$TOP_DIR/src/" --download "$TOP_DIR/src/freeze_core/include/"
+popd >/dev/null || exit
