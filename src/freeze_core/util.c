6935707cc2f30df22478688ea403851a5da284dd
Marcelo Duarte <marcelotduarte@users.noreply.github.com>
2025-10-18 05:59:15 -0300

    typo

--- src/freeze_core/util.c
+++ src/freeze_core/util.c
@@ -561,7 +561,7 @@ PyMODINIT_FUNC PyInit_util(void)
     if (PyModule_AddObject(module, "BindError", g_BindErrorException) < 0)
         return NULL;
 #ifdef Py_GIL_DISABLED
-    PyUnstable_Module_SetGIL(m, Py_MOD_GIL_NOT_USED);
+    PyUnstable_Module_SetGIL(module, Py_MOD_GIL_NOT_USED);
 #endif
     return module;
 }
