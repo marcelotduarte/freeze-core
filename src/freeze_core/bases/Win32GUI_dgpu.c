b9f7e8e2c16a519ec44fa3d95bc97c241a7b8283
Aleksandar Topuzović <aleksandar.topuzovic@gmail.com>
2026-01-29 20:32:51 +0100

    Add GPU acceleration hints for Windows GUI base (#89)
    * Add GPU acceleration hints for Windows GUI base
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    * Remove legacy
    
    * Links to documentation
    
    * Better naming
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ src/freeze_core/bases/Win32GUI_dgpu.c
@@ -0,0 +1,8 @@
+#include "Win32GUI.c"
+
+// GPU acceleration hints
+// https://developer.download.nvidia.com/devzone/devcenter/gamegraphics/files/OptimusRenderingPolicies.pdf
+__declspec(dllexport) DWORD NvOptimusEnablement = 0x00000001;
+
+// https://gpuopen.com/learn/amdpowerxpressrequesthighperformance/
+__declspec(dllexport) DWORD AmdPowerXpressRequestHighPerformance = 0x00000001;
